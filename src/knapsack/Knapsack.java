52dab33f641584283dfd930ddabbb6aa44a94ed2
Mikkel Riise Lund <lund.mikkel@gmail.com>
2014-11-05 10:32:12 +0100

    Reformats KnapSack to Mikkel's pleasing ;)

--- src/knapsack/Knapsack.java
+++ src/knapsack/Knapsack.java
@@ -6,20 +6,19 @@ import java.util.*;
  * Created by Rasmus on 31-10-2014.
  */
 public class Knapsack {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         List<Item> items = new ArrayList<>();
-                          //id ,W,v
-        items.add(new Item("1", 1,2));
-        items.add(new Item("2", 1,2));
-        items.add(new Item("3", 2,4));
-        items.add(new Item("4", 1,2));
-        items.add(new Item("5", 1,2));
-        items.add(new Item("6", 2,4));
-        items.add(new Item("7", 1,2));
-        items.add(new Item("8", 1,2));
-        items.add(new Item("9", 2,4));
-        items.add(new Item("10", 2,3));
+        //                  id,  W, v
+        items.add(new Item(" 1", 1, 2));
+        items.add(new Item(" 2", 1, 2));
+        items.add(new Item(" 3", 2, 4));
+        items.add(new Item(" 4", 1, 2));
+        items.add(new Item(" 5", 1, 2));
+        items.add(new Item(" 6", 2, 4));
+        items.add(new Item(" 7", 1, 2));
+        items.add(new Item(" 8", 1, 2));
+        items.add(new Item(" 9", 2, 4));
+        items.add(new Item("10", 2, 3));
 
         int W = 9;
         System.out.println("Input: W = " + W);
@@ -34,37 +33,42 @@ public class Knapsack {
         System.out.println();
     }
 
-    public static <T extends Knapsackable> Set<T> knapsack(List<T> items, int W)
-    {
-        System.out.println("Allocating of size: " + (4*((W+1) * (items.size()+1))) + " Byte");
-        double[][] cache = new double[items.size()+1][W+1];
-        System.out.println("Array allocated of size: " + ((W+1) * (items.size()+1)));
-        for (int item = 1; item <= items.size(); item++){
-            for (int w = 0; w <= W; w++)
-            {
-                Knapsackable i = items.get(item-1);
-                if (i.getWeight() > w) continue;
-                cache[item][w] = Math.max(cache[item-1][w], i.getValue() + cache[item-1][w-i.getWeight()]);
+    public static <T extends Knapsackable> Set<T> knapsack(List<T> items, int W) {
+        System.out.println("Allocating of size: " + (4 * ((W + 1) * (items.size() + 1))) + " Byte");
+
+        double[][] cache = new double[items.size() + 1][W + 1];
+        for (int item = 1, size = items.size(); item <= size; ++item) {
+            for (int w = 0; w <= W; ++w) {
+                T i = items.get(item - 1);
+                if (i.getWeight() <= w) {
+                    double notSelected = cache[item - 1][w];
+                    double selected = i.getValue() + cache[item - 1][w - i.getWeight()];
+                    cache[item][w] = Math.max(notSelected, selected);
+                }
             }
         }
-        /*for (int item = 0; item <= items.size(); item++)
-        {
-            if (item == 0) {
-                System.out.println("Item w: - v: - : " + Arrays.toString(cache[item]));
-                continue;
-            }
-            Knapsackable k = items.get(item-1);
-            System.out.print("Item w: " + k.getWeight() + " v: " + k.getValue() + " : ");
-            System.out.println(Arrays.toString(cache[item]));
-        }
-        System.out.println();*/
+
+        //for (int item = 0; item <= items.size(); item++)
+        //{
+        //if (item == 0) {
+        //System.out.println("Item w: - v: - : " + Arrays.toString(cache[item]));
+        //continue;
+        //}
+        //Knapsackable k = items.get(item-1);
+        //System.out.print("Item w: " + k.getWeight() + " v: " + k.getValue() + " : ");
+        //System.out.println(Arrays.toString(cache[item]));
+        //}
+        //System.out.println();
+
         Set<T> result = new HashSet<>();
-        for (int item = items.size(), w = W; item > 0 && w > 0; item--)
-        {
-            if (items.get(item-1).getWeight() > w) continue;
-            if (cache[item-1][w-items.get(item-1).getWeight()] == cache[item][w]-items.get(item-1).getValue()) {
-                w -= items.get(item - 1).getWeight();
-                result.add(items.get(item - 1));
+        for (int item = items.size(), w = W; 0 < item; --item) {
+            if (items.get(item - 1).getWeight() <= w) {
+                double actual = cache[item - 1][w - items.get(item - 1).getWeight()];
+                double expected = cache[item][w] - items.get(item - 1).getValue();
+                if (actual == expected) {
+                    w -= items.get(item - 1).getWeight();
+                    result.add(items.get(item - 1));
+                }
             }
         }
         return result;
