546aaa933b0231cb1eee613c6914cef2e23107f0
Mikkel Riise Lund <lund.mikkel@gmail.com>
2014-12-10 10:05:00 +0100

    Merges with last commit.

--- SAD2/SimpleMapReduce/src/ActorRating/GraphBuildMapper.java
+++ SAD2/SimpleMapReduce/src/ActorRating/GraphBuildMapper.java
@@ -12,25 +12,23 @@ public class GraphBuildMapper implements Mapper<String, String, Integer, Tuple<F
 
     @Override
     public void map(String fileName, String fileContent, Collector<Integer, Tuple<Float, Integer>> collector) {
-        StringTokenizer tokenizer = new StringTokenizer(fileContent);
-
-        //Parse id and rating
-        //int movieID = Integer.parseInt(tokenizer.nextToken());
-        tokenizer.nextToken(); //skip movie ID
-
-        float movieRating = Float.parseFloat(tokenizer.nextToken()) / 10;
-
-        //Parse all actors
-        List<Integer> actors = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(fileContent, "\r\n");
         while (tokenizer.hasMoreTokens()){
-            actors.add(Integer.parseInt(tokenizer.nextToken()));
-        }
-
-        //Generate graph
-        for (int actorA : actors){
-            actors.stream().filter(actorB -> actorA != actorB).forEach(actorB -> {
-                collector.collect(actorA, new Tuple<>(movieRating, actorB));
-            });
+            String line = tokenizer.nextToken();
+            System.out.println("Reading line: " + line);
+            StringTokenizer lineTokenizer = new StringTokenizer(line, ",");
+            float movieRating = Float.parseFloat(lineTokenizer.nextToken()) / 10;
+            //Parse all actors
+            List<Integer> actors = new ArrayList<>();
+            while (lineTokenizer.hasMoreTokens()){
+                actors.add(Integer.parseInt(lineTokenizer.nextToken()));
+            }
+            //Generate graph
+            for (int actorA : actors){
+                actors.stream().filter(actorB -> actorA != actorB).forEach(actorB -> {
+                    collector.collect(actorA, new Tuple<>(movieRating, actorB));
+                });
+            }
         }
     }
 }
